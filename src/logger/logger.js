b59ae5dfebf71f66a4a89b515f94382aace7909a
sachinp2906 <sachinp.2906@gmail.com>
2022-10-17 14:04:58 +0530

    the files attached for assignment purpose

---
+++ src/logger/logger.js
@@ -0,0 +1,8 @@
+function welcome() {
+    console.log(
+      "Welocme to My Application. I am Rupam Gupta and a part of FunctionUp Lithium Cohort "
+    );
+  }
+  
+  module.exports.welcome = welcome;
+  
