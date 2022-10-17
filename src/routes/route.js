9439edb26477b41b4252c413242c0efd78c550a3
sachinp2906 <sachinp.2906@gmail.com>
2022-10-17 22:24:45 +0530

    only changes in route.js file

--- src/routes/route.js
+++ src/routes/route.js
@@ -40,4 +40,85 @@ router.get('/student-details/:name', function(req, res){
     res.send('Dummy response')
 })
 
-module.exports = router;
+//problem 1 
+router.get('/movies1' , function(req ,res) { 
+    let movies = ["Rang de Basanti" , "The Shining" , "Lord of the rings " , "Batman Begins"]
+    //console.log("The List Of The Movies",movies)
+    res.send("List of movies " + movies)
+    
+    })
+    // problem 2
+    router.get('/movies2/:indexNumber' , function(req,res){
+        const movie$arr = ["Rang De Basanti" , "The Shining" , "Lord of the rings" , "Batman Begins"]
+        const index = req.params.indexNumber
+        console.group("print movie name using index" , movie$arr)
+        res.send (movie$arr[index])
+    })
+    
+
+    // problem 3
+    router.get('/movies/:index',function(req,res){
+        const myParam=req.params
+        const moviesList=["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+        
+        const myIndex=myParam.index
+        if(myIndex>3 || myIndex<0){
+            res.send("Your index is invalid ")
+        
+        }
+        else
+        {
+            res.send(moviesList[myIndex])
+        }
+    })
+    // Problem 4
+    router.post('/films',function(req,res){
+        const filmsObject=[ {
+            "id": 1,
+            "name": "The Shining"
+           }, {
+            "id": 2,
+            "name": "Incendies"
+           }, {
+            "id": 3,
+            "name": "Rang de Basanti"
+           }, {
+            "id": 4,
+            "name": "Finding Nemo"
+           }]
+    
+           res.send(filmsObject)
+           
+    })
+    // Problem 5
+    router.get('/films/:filmId',function(req,res){
+        const myParam=req.params
+        const filmsObject=[ {
+            "id": 1,
+            "name": "The Shining"
+           }, {
+            "id": 2,
+            "name": "Incendies"
+           }, {
+            "id": 3,
+            "name": "Rang de Basanti"
+           }, {
+            "id": 4,
+            "name": "Finding Nemo"
+           }]
+    
+          
+           for(let i=0;i<filmsObject.length;i++){
+            const myId=myParam.filmId
+            if(i==myId-1 ){
+                res.send(filmsObject[i])
+                break;
+    
+            }
+            else if(i==filmsObject.length-1){
+                res.send("no such movie with this id")
+               }
+               }
+            })
+    
+    module.exports = router;
