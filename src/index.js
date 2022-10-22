99879f00991651a4b4220adb5ff5bfe85dc30355
sachinp2906 <sachinp.2906@gmail.com>
2022-10-22 17:42:20 +0530

    bookSchema Assignment Update

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://sachinfu:2906@sachinfu.fcpe6tc.mongodb.net/sachinfu-db", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -19,4 +19,4 @@ app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
-});
+});  
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+const BookModel = require('../models/bookModel')
+const bookController = require('../controllers/bookController')
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
+// router.get("/test-me" , function(req ,res) {
+//     res.send("HAR HAR MAHADEV")
+// })
+router.post('/createBook' , bookController.createBook )
+router.get('/findBook' , bookController.findBook )
 
 module.exports = router;
